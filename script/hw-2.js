f9ece8de365f0a469c00d76176003c59d1343780
noraaranara <anastasiya.solntseva@gmail.com>
2025-10-21 23:04:47 +0300

    finished adaptation

---
+++ script/hw-2.js
@@ -0,0 +1,65 @@
+// 1)
+let number = 10;
+alert (number);
+// 1.2)
+number = 20;
+alert (number);
+
+// 2)
+let iphone = 2007;
+alert (iphone);
+
+// 3)
+let js = "Brendan-Eich";
+alert (js);
+
+// 4)
+let small = 2;
+let big = 10;
+
+let sum = small + big;
+alert (sum);
+
+let difference = big - small;
+alert (difference);
+
+let product = big * small;
+alert (product);
+
+let quotient = big / small;
+alert (quotient);
+
+// 5)
+let result = 2 ** 5;
+alert (result);
+
+// 6)
+let a = 9;
+let b = 2;
+let remainder = a % b;
+alert (remainder);
+
+// 7)
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num++;
+num--;
+alert(num);
+
+// 8)
+let age = prompt("Сколько вам лет?");
+alert(age);
+
+// 9)
+const user = {
+    name: 'Nora',
+    age: 19,
+    isAdmin: true
+}
+
+// 10)
+let name = prompt(`Введите ваше имя:`);
+alert (`Привет, ${name}!`);
